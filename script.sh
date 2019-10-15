3b08e5e8cc142269c0d332ffe0d1628b39e5ddb8
Lucifer K. Dangol <kiran_dan2009@yahoo.com>
2019-10-15 09:51:35 -0500

    Testing more commands, more snippets, now with more data

--- script.sh
+++ script.sh
@@ -1,2 +1,8 @@
-echo "My First Script!"
+# NAME="Kiran Dangol"
+# echo "My name is $NAME"
 
+# echo $(ls -al) > data.txt
+# echo SERVER_NAME=$(hostname)
+
+read -p "Please Enter your name : " NAME
+echo "Thank you, it's nice to meet you, $NAME"
