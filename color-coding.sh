df91a444f2cf90a5ca777bce6b4698bbc521ccf3
Lucifer K. Dangol <kiran_dan2009@yahoo.com>
2019-10-17 13:26:28 -0500

    Color coding inside bash shell terminal

---
+++ color-coding.sh
@@ -0,0 +1,23 @@
+# Blue on Green
+echo -e "\033[34;42mBlue on Green\033[0m"
+
+# White on Black
+echo -e "\033[37;40mWhite on Black\033[0m"
+
+# Yellow on Green
+echo -e "\033[43;42mYellow on Green\033[0m"
+
+# Black on Red
+echo -e "\033[30;41mBlack on Red\033[0m"
+
+# Green on Black
+echo -e "\033[32;40mGreen on Black\033[0m"
+
+# Green on Red
+echo -e "\033[32;41mGreen on Red\033[0m"
+
+# Red on White
+echo -e "\033[31;47mRed on White\033[0m"
+
+# Blue on Yellow
+echo -e "\033[34;43mBlue on Yellow\033[0m"0
