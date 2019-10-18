d26bacd975f6875dadea9a6cee36d3aa68c41a6b
Lucifer K. Dangol <kiran_dan2009@yahoo.com>
2019-10-18 11:13:23 -0500

    dat sed lyf

--- sed.sh
+++ sed.sh
@@ -1,6 +1,14 @@
+# Following commands are equivalent:
+sed 's/hello/world/' input.txt > output.txt
+# sed 's/hello/world/' < input.txt > output.txt
+# cat input.txt | sed 's/hello/world/' - > output.txt
+
+
+# SED -> Stream Editor
+
 # Use case of sed
 # ./sed.sh /etc/passwd
-while IFS= read -r LINE; do
-    echo "$LINE" | sed 's/:/ | /g'
-done < $1
+# while IFS= read -r LINE; do
+#     echo "$LINE" | sed 's/:/ | /g'
+# done < $1
 # here sed will replace every occurence of : with | line by line
